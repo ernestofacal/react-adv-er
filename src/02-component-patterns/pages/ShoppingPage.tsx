21f69015fc8a829c06c2c3b9eaa0a2228e496fb4
Ernesto Facal <facerne123@gmail.com>
2022-07-31 11:59:37 -0300

    primeraSubida

---
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,37 @@
+import {
+  ProductCard,
+  ProductImage,
+  ProductTitle,
+  ProductButtons,
+} from "../components";
+
+import { products } from "../data/products";
+import "../styles/custom-styles.css";
+
+const product = products[0];
+
+export const ShoppingPage = () => {
+  return (
+    <div>
+      <h1>Shopping Store</h1>
+      <hr />
+
+      <ProductCard
+        key={product.id}
+        product={product}
+        initialValues={{
+          count: 6,
+          // maxCount: 10,
+        }}
+      >
+        {({ reset, count, isMaxCountReached, maxCount, increaseBy }) => (
+          <>
+            <ProductImage />
+            <ProductTitle />
+            <ProductButtons />
+          </>
+        )}
+      </ProductCard>
+    </div>
+  );
+};
